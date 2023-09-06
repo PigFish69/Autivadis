4588d56d9e1f8c7db5823670c3189410539ecd5b
Nuno_Garritsen <PigFish3@hotmail.com>
2023-09-06 14:35:37 +0200

    added 2 classes

---
+++ hyb/Actievadis/Class/User.php
@@ -0,0 +1,75 @@
+<?php
+class user {
+    private $id;
+    private $username;
+    private $password;
+    private $admin;
+
+    function __construct($sqlResult = null)
+    {
+        try {
+            if ($sqlResult)
+            {
+                $userArr = $sqlResult->fetch_row();
+                $this->id = $userArr[0];
+                $this->username = $userArr[1];
+                $this->password = $userArr[2];
+                $this->admin = $userArr[3];
+            } else {
+                return $this;
+            }
+            
+        } catch (\Throwable $th) {
+            //throw $th;
+            echo "Error: ".$th;
+        }
+        
+    }
+    
+    function setUser($id, $username, $password, $admin)
+    {
+        try {
+            $this->id = $id;
+            $this->username = $username;
+            $this->password = $password;
+            $this->admin = $admin;
+            return $this;
+        } catch (\Throwable $th) {
+            //throw $th;
+            echo "Error: ".$th;
+        }
+    }
+    
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of username
+     */ 
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Get the value of password
+     */ 
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Get the value of admin
+     */ 
+    public function getAdmin()
+    {
+        return $this->admin;
+    }
+}
