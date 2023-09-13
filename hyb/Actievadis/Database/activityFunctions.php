3bbf37d464499eabf355f51da25581d0dbcb65ca
Rosalie15V <rosalie.vleeming@student.graafschapcollege.nl>
2023-09-13 15:46:45 +0200

    index works inlog and signUp better

--- hyb/Actievadis/Database/activityFunctions.php
+++ hyb/Actievadis/Database/activityFunctions.php
@@ -5,13 +5,22 @@ require_once ("databaseFunctions.php");
 function getDateActivities($current, $next)
 {
     print_r("SELECT startTime FROM activity where startTime between '$current' and '$next'");    
-    return db_getData("SELECT startTime FROM activity where startTime between '$current' and '$next'");
+    
+    $startTime = db_getData("SELECT startTime FROM activity where startTime between '$current' and '$next'");
+    if ($startTime->num_rows > 0){
+        return $startTime;
+    }
+    return "No startTime found!";
 
 }
 
-function getNameActivities($current, $next)
+function getActivitysBetweenTime($current, $next)
 {
-    return db_getData("SELECT name FROM activity where startTime between '$current' and '$next'"); 
+    $activities = db_getData("SELECT * FROM activity where startTime between '$current' and '$next'");
+    if ($activities->num_rows > 0){
+        return $activities;
+    }
+    return "No activities found!"; 
 }
 
 ?>
