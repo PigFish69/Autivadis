9d0d63bab1fad7d4d90d71650d626bae4a64b956
Rosalie15V <rosalie.vleeming@student.graafschapcollege.nl>
2023-09-13 15:52:43 +0200

    delete useless function

--- hyb/Actievadis/Database/activityFunctions.php
+++ hyb/Actievadis/Database/activityFunctions.php
@@ -2,18 +2,6 @@
 
 require_once ("databaseFunctions.php"); 
 
-function getDateActivities($current, $next)
-{
-    print_r("SELECT startTime FROM activity where startTime between '$current' and '$next'");    
-    
-    $startTime = db_getData("SELECT startTime FROM activity where startTime between '$current' and '$next'");
-    if ($startTime->num_rows > 0){
-        return $startTime;
-    }
-    return "No startTime found!";
-
-}
-
 function getActivitysBetweenTime($current, $next)
 {
     $activities = db_getData("SELECT * FROM activity where startTime between '$current' and '$next'");
