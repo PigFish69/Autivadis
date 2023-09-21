4698fa82399eb52c793a45440964aa07436344d9
Rick Snijders <97365268+LoneOmegaWolf@users.noreply.github.com>
2023-09-21 11:19:31 +0200

    Changed table to cards. Basic styling

--- hyb/Actievadis/Pages/Activities/Overview.php
+++ hyb/Actievadis/Pages/Activities/Overview.php
@@ -31,40 +31,57 @@ function registerForActivity($userId, $activityId)
 <html>
 <link rel="stylesheet" href="../../Css/Overview.css">
 
-<!-- Link table relations -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-<link href="//cdn.datatables.net/1.13.3/css/jquery.dataTables.min.css" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 
 
 <body>
-    <!-- First give an overview via an Table of all upcoming activities -->
+
+    <div class="containerCards">
+        <h1 class="bold">Activiteiten</h3>
+            <div class="activityCards">
+                <?php
+                $activity = db_getData("SELECT * FROM `activity`");
+                while ($activityData = $activity->fetch_assoc()) {
+                ?>
 
 
-    <div class="containerTable">
-        <h3 class="bold">Activiteiten</h3>
-        <table id="allActivities" class="table table-striped tableActivities">
+                    <div class="card" id="<?php echo $activityData['id'] ?>">
+                        <img src="https://maken.wikiwijs.nl/generated/s960x720_3eb68a0fc0f8354d440713a2ed902b657cac8ef2.jpg" />
+                        <div class="cardText">
+                            <h3><?php echo $activityData['name'] ?></h3>
+                            <p>Begin tijd: <?php echo $activityData['startTime'] ?></p>
+                            <p>Kosten: <?php echo "€" . number_format((float)$activityData['price'], 2, '.', '') ?></p>
+                        </div>
+                    </div>
+
+
+                <?php
+                }
+                ?>
+            </div>
+
+            <!-- ------TABLE HAS BEEN REJECTED ^Going to be cards^ -->
+            <!--<table id="allActivities" class="table table-striped tableActivities">
             <thead>
                 <tr>
                     <th>Naam Activiteit</th>
                     <th>Locatie</th>
                     <th>Eten Inbegrepen</th>
-                    <!-- <th>Maximaal Aantal Deelnemers</th> Should-->
-                    <!-- <th>Minimaal Aantal Deelnemers</th> Should-->
+                    <th>Maximaal Aantal Deelnemers</th> Should
+                    <th>Minimaal Aantal Deelnemers</th> Should
                     <th>Kosten</th>
-                    <!-- <th>Benodigheden</th> Could-->
+                    <th>Benodigheden</th> Could
                     <th>Omschrijving</th>
                     <th>Begin Tijd</th>
                     <th>Eind Tijd</th>
-                    <!-- <th>Afbeelding</th> Should-->
+                    <th>Afbeelding</th> Should
                 </tr>
             </thead>
 
-            <!-- Get Activity data from Database and fill tables -->
-            <!-- Add Button for Sign up / Possibly remove activitie once Signed up for-->
+            Get Activity data from Database and fill tables
+            Add Button for Sign up / Possibly remove activitie once Signed up for
 
             <tbody>
-                <!-- Sample Data -->
+                Sample Data
                 <?php
                 $users = db_getData("SELECT * FROM `activity`");
                 while ($userData = $users->fetch_assoc()) { ?>
@@ -79,31 +96,16 @@ function registerForActivity($userId, $activityId)
                         <td>
                             <button class="btnSignup" id="<?php echo $userData['id'] ?>"><i class="bi bi-envelope-plus"></i></button>
                         </td>
-                        <!-- <td>Laser Gaming</td>
-                        <td>Achtertuin Jan</td>
-                        <td>Nee</td>
-                        <td>€10,55</td>
-                        <td>We gaan weer eens laser gamen</td>
-                        <td>12.00</td>
-                        <td>17.00</td> -->
-                    </tr>
-                    <!-- <tr>
-                        <td>BBQ</td>
-                        <td>Achtertuin Peter </td>
-                        <td>Ja</td>
-                        <td>€29.99</td>
-                        <td>We gaan lekker eten</td>
-                        <td>16.00</td>
-                        <td>20.00</td>
 
-                    </tr> -->
+                    </tr>
                 <?php
                 }
                 ?>
-            </tbody>
+            </tbody> -->
     </div>
 
 </body>
+<!-- <?php include "../General/footer.php" ?> -->
 
 <!-- Show all Activities. Which you signed up for -->
 <!-- Button for Sign out from activitie -->
